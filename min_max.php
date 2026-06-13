3ba16ececaa5e6f738e8fdf645cb64b46c960b0b
batata <contactobrite@gmail.com>
2026-06-13 20:51:59 +0000

    numbers

--- min_max.php
+++ min_max.php
@@ -4,10 +4,10 @@
 // MIN & MAX
 // ===============================
 
-$numbers = [5, 2, 9, 1, 7];
+$numbers = [5, 2, 9, 1, 7, 0, -15];
 
 // valor mínimo
-echo min($numbers) . "\n"; // 1
+echo min($numbers) . "\n"; // -15
 
 // valor máximo
 echo max($numbers) . "\n"; // 9
